a80d001732829095844654cca491fb9a1ec3ae0c
coloripop <coloripop@gmail.com>
2018-10-20 16:33:21 -0400

    Random images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,5 @@ export class AppComponent {
     ceil: 200
   };
   //images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
-  images = [1, 2, 3].map(() => `https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2012-07-09/250m/6/13/36.jpg`);  
+  images = [1, 2, 3].map(() => `https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2012-07-09/250m/6/13/${Math.floor((Math.random()*40)+1)}.jpg`);
 }
