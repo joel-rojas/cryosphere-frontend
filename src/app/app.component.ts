bb13827413590a19114f5d4b44b948983ac2ab24
coloripop <coloripop@gmail.com>
2018-10-20 16:23:44 -0400

    Map dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,6 @@ export class AppComponent {
     floor: 0,
     ceil: 200
   };
+  //images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  images = [1, 2, 3].map(() => `https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2012-07-09/250m/6/13/36.jpg`);  
 }
